29ba63eda09f9d176a7473b40f1968e80cbbcc44
Justin Dennison <justin1dennison@gmail.com>
2017-07-21 19:44:39 -0400

    created and wired up the post router that will allow us manipulation blog posts through API calls

---
+++ routers/post.router.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/posts', (req, res) => {
+	res.send('getting those posts');
+});
+
+router.get('/posts/:postId', (req, res) => {
+	res.send('getting that one post');
+});
+
+router.post('/posts', (req, res) => {
+	res.send('creating a new post');
+});
+
+router.put('/posts/:postId', (req, res) => {
+	res.send('updates on the post!');
+});
+
+router.delete('/posts/:postId', (req, res) => {
+	res.send('deleting that one post'); 
+});
+
+module.exports = router;
