be89045d1680792799c15ad5ab14b7af0add444e
Justin Dennison <justin1dennison@gmail.com>
2017-07-26 20:23:17 -0400

    added the body-parser middleware in order to allow user requests to send additional data. Fully implement the CRUD operations through the HTTP endpoints thus giving moderated access to the database.

--- routers/user.router.js
+++ routers/user.router.js
@@ -3,23 +3,49 @@ const router = express.Router();
 const User = require('../models/user.model');
 
 router.get('/users', (req, res) =>{
-  res.send('getting all dem users!!');
+  // the {} is the query
+  User.find({}, function(err, users){
+    if(err) return res.status(500).json({err: err});
+    return res.status(200).json({
+      users: users
+    });
+  });
 });
+// get that one user by the _id
 router.get('/users/:userId', (req, res)=>{
-  res.send('getting that one special someone!');
+  User.find({ _id : req.params.userId }, function(err, users){
+    if(err) return res.status(500).json({err: err});
+    return res.status(200).json({
+      users: users
+    });
+  });
 });
 router.post('/users', (req, res) => {
-  const newUser = new User({ email: 'j@j.j' });
-  newUser.save(function(){
-    res.send('Created a new user');
+  const newUser = new User(req.body);
+  newUser.save(function(err, user){
+    if(err) return res.status(500).json({err: err});
+    return res.status(201).json({
+      msg: "Successfully created user"
+    });
   });
+});
 
-});
 router.put('/users/:userId', (req, res) =>{
-  res.send('updates errrrywhere!');
+  User.findOneAndUpdate({ _id: req.params.userId}, req.body, function(err){
+    if(err) return res.status(500).json({err: err});
+    return res.status(200).json({
+      msg: "Successfully updated user"
+    });
+  });
 });
+
 router.delete('/users/:userId', (req, res) =>{
-  res.send('burning it down!');
+  User.findOneAndRemove({ _id: req.params.userId}, function(err, removedUser){
+    if(err) return res.status(500).json({err: err});
+    return res.status(200).json({
+      msg: "User successfully removed"
+    });
+  });
 });
 
 module.exports = router;
